e05317b00df50d27d50dddf148c5184572cf1a52
Jaroslaw Wisniewski <jaroswi@gmail.com>
2018-06-03 16:26:21 +0100

    Task 3 - Finished Comments Time Overlapping circles removed

--- Task_3_Eye_Detection.py
+++ Task_3_Eye_Detection.py
@@ -1,14 +1,10 @@
 from __future__ import print_function
 from skimage import data
-from skimage.color import rgb2gray, gray2rgb
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.transform import hough_circle, hough_circle_peaks, hough_ellipse
-from skimage.feature import peak_local_max, canny
-from skimage.draw import circle_perimeter, ellipse_perimeter, circle
-from skimage.morphology import watershed
-import matplotlib.image as mpimg
-from scipy import ndimage as ndi
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.feature import canny
+from skimage.draw import circle_perimeter, circle
 
 from skimage import feature
 from skimage import io
@@ -36,10 +32,8 @@ n_samples, h, w = lfw_people.images.shape
 # for machine learning we use the 2 data directly (as relative pixel
 # positions info is ignored by this model)
 X = lfw_people.data
-print("X shape is {}".format(X.shape))
-#for x in enumerate(X):
- #   z[x]=X[x].shape(h, w)
 
+# number of features (pixels)
 n_features = X.shape[1]
 
 # the label to predict is the id of the person
@@ -52,32 +46,56 @@ print("n_samples: %d" % n_samples)
 print("n_features: %d" % n_features)
 print("n_classes: %d" % n_classes)
 
-def plot_gallery(images, h, w, n_row=3, n_col=4):
+def plot_gallery(images, h, w, n_row=4, n_col=5):
     """Helper function to plot a gallery of portraits"""
     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
     plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
     for i in range(n_row * n_col):
         plt.subplot(n_row, n_col, i + 1)
-        img = images[i].reshape(h, w)
-        markers1 = np.zeros_like(img)
-        markers1[img[:,:] > 80] = 1
 
+        # reshape data - form images (height, width)
+        img = images[i].reshape(h, w)
+
+        # create mask covering only grey pixels with value below 80 (mask might not work with strong lighting)
+        markers1 = np.zeros_like(img)
+        markers1[img[:,:] < 80] = 1
+
+        # create mask marking already selected circles (removes overlapping circles)
+        markers2 = np.zeros_like(img)
+
+        # apply canny edge detection on the image file
         edges = canny(img, sigma=3)
-                
+
+        # search for circles
         hough_radii = np.arange(3, 30, 1)
         hough_res = hough_circle(edges, hough_radii)
+        # get x, y, r value of each circle
         accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
                                            total_num_peaks=8)
+
                                           
         for center_y, center_x, radius in zip(cy, cx, radii):
+            # get array of pixels covered by the circles with half radius, on the masked image
             rr, cc = circle(center_y, center_x, radius/2, markers1.shape)
-            if np.any(rr<h/2):
-                if np.any(markers1[rr, cc] == 0):
-                    circy, circx = circle_perimeter(center_y, center_x, radius)
-                    img[circy, circx] = (255)
+            # remove the circles located below 2/5 height of the image
+            if np.any(rr<h*2/5):
+                # if covered pixels grey values is below 80 (marker1)
+                if np.any(markers1[rr, cc] == 1):
+                    # get array of pixels covered by new circle
+                    yy, xx = circle(center_y, center_x, radius, markers2.shape)
+                    # if circle is NOT overlapping any previously markes circles
+                    if np.all(markers2[yy, xx] == 0):
+                        # include the circle covered pixels to the mask
+                        yy, xx = circle(center_y, center_x, radius, markers2.shape)
+                        markers2[yy, xx] = 1
+                        # mark circle
+                        circy, circx = circle_perimeter(center_y, center_x, radius)
+                        img[circy, circx] = (255)
+                        
         plt.imshow(img, cmap=plt.cm.gray)
         plt.xticks(())
         plt.yticks(())
-
-plot_gallery(X, h, w) 
+start = time()
+plot_gallery(X, h, w)
+print("Eye detection time = {:.2f} seconds".format(time() - start))
 plt.show()
